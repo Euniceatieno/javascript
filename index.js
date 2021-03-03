f62f9b3a801c83d12c771059c38341f0c3ac352d
Euniceatieno <eunniceatieno@gmail.com>
2021-03-03 10:35:42 +0300

    First js

---
+++ index.js
@@ -0,0 +1,109 @@
+
+console.log("HELLO WORLD");
+var number1=50;
+var number2=20;
+var result=number1+number2;
+console.log(result);
+var me="I am Eunice";
+alert(me);
+let name="Eunice";
+console.log(name);
+let birthRate=0.3;
+birthRate=1;
+console.log(birthRate);
+let age=50;
+console.log(age);
+let firtsName;
+let isLoggedIn=true;
+console.log(isLoggedIn);
+let myAge=null;
+console.log(myAge);
+let student = {
+    name:"Eunice",
+    age:50
+    
+
+
+};
+    console.log(student);
+    let studentNames=["jerry","mary"];
+    console.log(studentNames);
+    console.log(studentNames[0]);
+    console.log(studentNames[1]);
+
+
+
+let close="Close the page.";
+prompt(close);
+let number5=50;
+--number5;
+console.log(number5);
+
+let sum=addition(4,3)
+function addition(number6,number7){
+     return number6+number7;
+}
+console.log(sum);
+
+let multiplication=multiply(7,4)
+function multiply(number8,number9){
+    return number8*number9;
+}
+console.log(multiplication);
+
+ let division=divide(20,5)
+ function divide(number10,number20){
+     return number10/number20;
+ }
+ console.log(division);
+
+let modulus=remainder(32,5)
+function remainder(numberOne,numberTwo){
+    return numberOne%numberTwo;
+}
+console.log(modulus);
+
+ let subtraction=minus(9,7)
+ function minus(numberTen,numberNine){
+     return numberTen-numberNine;
+ }
+console.log(minus);
+
+let addd=summ(9,7)
+function summ(num1,num2){
+    return num1+num2;
+}
+    console.log(summ);
+
+    const number=prompt("Enter a number: ")
+    if(number<0){
+        console.log("The number is negative");
+    }
+    const age1=prompt("Enter age: ")
+    if(age1>18){
+        console.log("Fit for driving");
+    }
+
+ const age2=alert("Unfit for school")
+ if(age2>29){
+     console.log("Cancel");
+ }
+ const age3=prompt("ENTER AGE: ")
+ if(age3<18){
+     console.log("Can go partying");
+ }
+ else{
+     console.log("Lock in the house");
+ }
+ const nums=20;
+ for(let i=1; i<=nums; i++){
+     console.log(i);
+ }
+ const nums2=3;
+ for(let first=1; first<=3; first++){
+     console.log(first);
+ }
+
+
+
+
